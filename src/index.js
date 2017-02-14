fe958271bfa3e3dddf4f144ea3b3298724ed798f
Meredith Nachman <mwnachman@gmail.com>
2017-02-13 20:45:08 -0800

    change to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory as history } from 'react-router';
+import { hashHistory as history } from 'react-router';
 import Routes from './routes';
 import './index.css';
 
